a38a6f85f01f070a17dfd61523df5cc13282a744
KuristinaMakise <duquesne.alexandre.l@gmail.com>
2018-02-22 01:24:25 +0100

    Evol
    Added config.php for API_KEY const + new call get_match_by_id

--- API/get_static_champions.php
+++ API/get_static_champions.php
@@ -1,6 +1,7 @@
 <?php
 //  Include all required files
-require_once $_SERVER['DOCUMENT_ROOT']  . "/vendor/autoload.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/vendor/autoload.php";
+require_once $_SERVER['DOCUMENT_ROOT'].'/config.php';
 header("Access-Control-Allow-Origin: *");
 use RiotAPI\RiotAPI;
 use RiotAPI\Definitions\Region;
@@ -8,7 +9,7 @@ use RiotAPI\Definitions\Region;
 //  Initialize the library
 $api = new RiotAPI([
 	//  Your API key, you can get one at https://developer.riotgames.com/
-	RiotAPI::SET_KEY    => 'RGAPI-1bb98110-bab7-4058-a9c9-9496fdfb49eb',
+	RiotAPI::SET_KEY    => API_KEY,
 	RiotAPI::SET_VERIFY_SSL => false,
 	//  Target region (you can change it during lifetime of the library instance)
 	RiotAPI::SET_REGION => Region::EUROPE_WEST,
---
+++ config.php
@@ -0,0 +1,2 @@
+<?php
+define('API_KEY', 'RGAPI-1bb98110-bab7-4058-a9c9-9496fdfb49eb');
