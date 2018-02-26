2931bc88777fc653c72ec5ee590ecb0a0a31c41c
KuristinaMakise <duquesne.alexandre.l@gmail.com>
2018-02-26 23:26:27 +0100

    test DB

--- index.php
+++ index.php
@@ -4,13 +4,12 @@
 # string suitable for pg_connect. Put this in your app.
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["DATABASE_URL"]));
-  var_dump("user=$user password=$pass host=$host dbname=" . substr($path, 1));
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 # Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 # Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+$result = pg_query($pg_conn, "SELECT * FROM label");
 print "<pre>\n";
 if (!pg_num_rows($result)) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
